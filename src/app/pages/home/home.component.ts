604ff3efcba7c2820f5ecdefa3f7aac16c9a7e4a
Megan Hugo <mhugo@dvtsoftware.com>
2024-11-18 14:16:01 +0200

    feat: :sparkles: Added home screen text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { Component } from '@angular/core';
+import { TextComponent } from './text/text.component';
+import { matDownload } from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, TextComponent],
+  viewProviders: [provideIcons({matDownload})],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
